dd70b5f530ea739f655f07d297d229826429a819
samuFerragut <sferragut@cifpfbmoll.eu>
2019-12-23 12:26:06 +0100

    feat: Crear modulo Item, clases Item y NormalItem

---
+++ app/Item.py
@@ -0,0 +1,17 @@
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class NormalItem(Item):
+    def set_sellIn(self):
+        self.sell_in - 1
+
+    def update_quality(self):
+        if self.quality < 50 and self.quality > 0:
+            self.quality - 1
