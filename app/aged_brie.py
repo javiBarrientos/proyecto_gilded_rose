bf7b2de6004c5673b1fbcf368d29fe7f8c81f711
samuFerragut <sferragut@cifpfbmoll.eu>
2019-12-26 17:08:58 +0100

    refactor: Eliminar objetos pasados innecesarios

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -1,13 +1,9 @@
 class Aged_brie(Normal_item):
-    def __init__(self, update_quality, sell_in):
-        self.update_quality = update_quality
-        self.sell_in = sell_in
-
-    def update_quality(update_quality, sell_in):
+    def update_quality(quality, sell_in):
         if sell_in >= 0:
-            return update_quality + 1
+            return quality + 1
         else:
-            return update_quality + 2
+            return quality + 2
 
 
 if __name__ == "__main__":
--- app/sulfuras.py
+++ app/sulfuras.py
@@ -1,13 +1,9 @@
 class Sulfuras(Normal_item):
-    def __init__(self, update_quality, set_sell_in):
-        self.update_quality = update_quality
-        self.set_sell_in = set_sell_in
+    def update_quality(self, quality):
+        return quality
 
-    def update_quality(self, update_quality):
-        return update_quality
-
-    def set_sell_in(self, set_sell_in):
-        return set_sell_in
+    def set_sell_in(self, sell_in):
+        return sell_in
 
 
 if if __name__ == "__main__":
