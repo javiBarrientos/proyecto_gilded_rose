4afc71941a862ca8d7a6911832ec94467cb863f4
samuFerragut <sferragut@cifpfbmoll.eu>
2019-12-26 16:49:53 +0100

    feat: Implementasr lógica de sulfuras

---
+++ app/sulfuras.py
@@ -0,0 +1,14 @@
+class Sulfuras(Normal_item):
+    def __init__(self, update_quality, set_sell_in):
+        self.update_quality = update_quality
+        self.set_sell_in = set_sell_in
+
+    def update_quality(self, update_quality):
+        return update_quality
+
+    def set_sell_in(self, set_sell_in):
+        return set_sell_in
+
+
+if if __name__ == "__main__":
+    pass
