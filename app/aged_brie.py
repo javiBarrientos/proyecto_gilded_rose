ca605552a23a0b49c108db81a762007fb061ee92
samuFerragut <sferragut@cifpfbmoll.eu>
2019-12-26 16:44:17 +0100

    fix: Definir __init__

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -1,9 +1,10 @@
-from item import Normal_item
-
-
 class Aged_brie(Normal_item):
-    def update_quality(self, update_quality, sell_in):
-        if self.sell_in >= 0:
+    def __init__(self, update_quality, sell_in):
+        self.update_quality = update_quality
+        self.sell_in = sell_in
+
+    def update_quality(update_quality, sell_in):
+        if sell_in >= 0:
             return update_quality + 1
         else:
             return update_quality + 2
