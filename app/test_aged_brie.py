33cb7870c7ad3df0a491def42d3e7f91d830d810
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 19:14:24 +0100

    test: Cambiar posicion test

--- app/test_aged_brie.py
+++ app/test_aged_brie.py
@@ -2,4 +2,4 @@ from aged_brie import Aged_brie
 
 
 def test_primer_caso():
-    assert Aged_brie.update_quality(0, 10) == Aged_brie.update_quality(50, -40)
+    assert Aged_brie.update_quality(50, -40) == Aged_brie.update_quality(0, 10)
