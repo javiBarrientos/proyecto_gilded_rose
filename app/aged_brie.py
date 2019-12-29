2b4b2c61ab0d37abeedbf3f0d3c1a07badaaaa1a
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 07:01:16 +0100

    refactor: Refactorizar codigo
    Cambiado iguales y la estructura interna del while

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -4,11 +4,15 @@ from gilded_rose import Normal_item
 class Aged_brie(Normal_item):
     def update_quality(update_quality, set_sell_in):
         while set_sell_in > 0:
-            update_quality = update_quality + 1
-            set_sell_in = set_sell_in - 1
+            if update_quality == 50:
+                update_quality += 0
+                set_sell_in -= 1
+            else:
+                update_quality += 1
+                set_sell_in -= 1
         else:
-            update_quality = update_quality + 2
-            set_sell_in = set_sell_in - 1
+            update_quality += 2
+            set_sell_in -= 1
 
         return update_quality
         return set_sell_in
