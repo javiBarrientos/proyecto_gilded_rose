9e6a475beb3e718521429dc160825ece3a47c5a2
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 10:07:48 +0100

    refactor: Cambiar variables
    He eliminado las variables update, set

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -18,13 +18,13 @@ class Item:
 
 
 class Normal_item(Item):
-    def __init__(self, update_quality, set_sell_in):
-        self.update_quality = update_quality
-        self.set_sell_in = set_sell_in
+    def __init__(self, quality, sell_in):
+        self.quality = quality
+        self.sell_in = sell_in
 
     def set_sell_in(self):
-        self.set_sell_in - 1
+        self.sell_in - 1
 
     def update_quality(self):
-        if self.update_quality < 50 and self.update_quality > 0:
-            self.update_quality - 1
+        if self.quality < 50 and self.quality > 0:
+            self.quality - 1
