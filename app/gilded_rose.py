79adf6b68fe87e7b7fab2d61ee5220bc1f808b54
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 18:19:49 +0100

    refactor: Cambiar posicion variables

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -18,9 +18,9 @@ class Item:
 
 
 class Normal_item(Item):
-    def __init__(self, quality, sell_in):
-        self.quality = quality
+    def __init__(self, sell_in, quality):
         self.sell_in = sell_in
+        self.quality = quality
 
     def set_sell_in(self):
         self.sell_in - 1
