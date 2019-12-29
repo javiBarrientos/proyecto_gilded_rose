6727ecc22b3b8805f4dd8960c821534ba3c9f5dc
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 06:33:05 +0100

    refactor: Eliminar acumulador

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -3,14 +3,11 @@ from gilded_rose import Normal_item
 
 class Aged_brie(Normal_item):
     def update_quality(update_quality, set_sell_in):
-        i = 0
         while i < set_sell_in:
             update_quality = update_quality + 1
-            i = i + 1
             set_sell_in = set_sell_in - 1
         else:
             update_quality = update_quality + 2
-            i = i + 1
             set_sell_in = set_sell_in - 1
 
         return update_quality
