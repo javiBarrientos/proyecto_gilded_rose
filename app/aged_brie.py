1c7258738917601afd9df20b70a50babcd5ceb6b
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 06:43:31 +0100

    refactor: Eliminar 'i' de while

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -3,7 +3,7 @@ from gilded_rose import Normal_item
 
 class Aged_brie(Normal_item):
     def update_quality(update_quality, set_sell_in):
-        while i < set_sell_in:
+        while set_sell_in > 0:
             update_quality = update_quality + 1
             set_sell_in = set_sell_in - 1
         else:
