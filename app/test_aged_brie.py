4a10c68986e80ea578e9520e739b20a5edd83e23
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 07:02:02 +0100

    test: Añadir test
    Añadido test dias negativos
    
    BREAKING CHANGE: Mal calculo de dias

--- app/test_aged_brie.py
+++ app/test_aged_brie.py
@@ -3,5 +3,8 @@ from aged_brie import Aged_brie
 
 def test_primer_caso():
     assert Aged_brie.update_quality(0, 10) == Aged_brie.update_quality(10, 0)
+
+
+def test_dias_negativos():
     assert Aged_brie.update_quality(
         20, -5) == Aged_brie.update_quality(40, -10)
