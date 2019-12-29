10e06c1f751d7339c0ed187ce4ae65181c88d3b1
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-29 08:41:47 +0100

    refactor: Eliminar test
    Queda mejorar la logica y añadir test

--- app/test_aged_brie.py
+++ app/test_aged_brie.py
@@ -2,8 +2,4 @@ from aged_brie import Aged_brie
 
 
 def test_primer_caso():
-    assert Aged_brie.update_quality(0, 10) == Aged_brie.update_quality(10, 0)
-
-
-def test_dias_negativos():
-    assert Aged_brie.update_quality(51, 30) == False
+    assert Aged_brie.update_quality(0, 10) == Aged_brie.update_quality(50, -40)
