76943a8c65766700a553df4581a8bb9f17281e01
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-30 10:07:36 +0100

    refactor: Añadir init

--- app/backstage_passes.py
+++ app/backstage_passes.py
@@ -1,11 +1,18 @@
+from gilded_rose import Normal_item
+
+
 class Backstage_passes(Normal_item):
-    def update_quality(self, quality, sell_in):
-        if sell_in < 10 and sell_in > 5:
-            quality + 2
-            return quality
-        if sell_in < 5 and sell_in > 0:
-            quality + 3
-            return quality
+    def __init__(self, sell_in, quality):
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def update_quality(self):
+        if self.sell_in < 10 and self.sell_in > 5:
+            self.quality + 2
+            return self.quality
+        if self.sell_in < 5 and self.sell_in > 0:
+            self.quality + 3
+            return self.quality
         else:
-            quality = 0
-            return quality
+            self.quality = 0
+            return self.quality
