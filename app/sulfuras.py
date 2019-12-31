751f19d17285b88b732aae5fb529c7f538a16341
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-31 13:53:57 +0100

    refactor: Cambiar quality, borrar name

--- app/sulfuras.py
+++ app/sulfuras.py
@@ -2,12 +2,12 @@ from gilded_rose import Normal_item
 
 
 class Sulfuras(Normal_item):
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, sell_in, quality):
         self.sell_in = sell_in
         self.quality = quality
 
     def update_quality(self):
-        assert self.update_quality == 50
+        assert self.update_quality == 80
         return self.quality
 
     def set_sell_in(self):
