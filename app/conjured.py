1bd8a68f90a1814abb2b3e375168d6f7f87bb172
Javi <jbarrientos@cifpfbmoll.eu>
2019-12-31 14:04:15 +0100

    feat: Crear conjured

---
+++ app/conjured.py
@@ -0,0 +1,19 @@
+from gilded_rose import Normal_item
+
+
+class Conjured(Normal_item):
+    def __init__(self, sell_in, quality):
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.quality - 2
+            return self.quality
+        else:
+            self.quality - 2
+            return self.quality
+
+    def set_sell_in(self):
+        self.sell_in - 1
+        return self.sell_in
