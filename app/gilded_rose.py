1a417fa1889f97b6aeb8881daace665f9e49a5a5
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-01 12:59:14 +0100

    refactor: Refactorizar y ampliar
    He añadido set_quality y he refactorizado update_quality

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -19,8 +19,19 @@ class Item:
 
 class Normal_item(Item):
     def set_sell_in(self):
-        self.sell_in - 1
+        self.sell_in -= 1
 
     def update_quality(self):
-        if self.quality < 50 and self.quality > 0:
-            self.quality - 1
+        if self.quality > 0:
+            self.set_quality(-1)
+        else:
+            self.set_quality(-2)
+        self.set_sell_in()
+
+    def set_quality(self, value):
+        if self.quality + value > 50:
+            self.quality = 50
+            if self.quality + value >= 0:
+                self.quality + value
+            else:
+                self.quality = 0
