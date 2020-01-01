1f2c3e2a218b68c105d939af6ff94eb70f588e26
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-01 12:42:26 +0100

    refactor: Eliminar init
    Eliminar init innecesario para el funcionamiento

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -2,10 +2,6 @@ from gilded_rose import Normal_item
 
 
 class Aged_brie(Normal_item):
-    def __init__(self, sell_in, quality):
-        self.sell_in = sell_in
-        self.quality = quality
-
     def update_quality(self):
         if self.sell_in > 0:
             self.quality + 1
