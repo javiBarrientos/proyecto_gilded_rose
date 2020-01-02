5dfff08baabb7f56c872f77c46691cb8523b7344
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-02 11:36:58 +0100

    test: Añadir test

--- app/test_conjured.py
+++ app/test_conjured.py
@@ -1,1 +1,7 @@
-from conjured import *
+from conjured import Conjured
+
+
+def test_primer_caso():
+    fetiche_sonrriente = Conjured("fetiche_sonrriente", 10, 0)
+    fetiche_sonrriente.update_quality()
+    fetiche_sonrriente.set_quality(9)
