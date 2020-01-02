e2081d2047b91c4ae86b28106465bc2fa9db4a32
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-02 11:19:18 +0100

    test: Arreglar test
    Ahora con ese formato los test funcionan

--- app/test_normal_item.py
+++ app/test_normal_item.py
@@ -4,4 +4,4 @@ from normal_item import Normal_item
 def test_primer_caso():
     maleta = Normal_item("maleta", 10, 0)
     maleta.update_quality()
-    assert maleta.set_quality(0)
+    maleta.set_quality(10)
