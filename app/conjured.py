9094cdef5c29e894cf6107d0b58438235c00bde9
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-02 11:34:44 +0100

    refactor: Cambiar import, quitar init

--- app/conjured.py
+++ app/conjured.py
@@ -1,11 +1,7 @@
-from gilded_rose import Normal_item
+from normal_item import Normal_item
 
 
 class Conjured(Normal_item):
-    def __init__(self, sell_in, quality):
-        self.sell_in = sell_in
-        self.quality = quality
-
     def update_quality(self):
         if self.sell_in > 0:
             self.quality - 2
