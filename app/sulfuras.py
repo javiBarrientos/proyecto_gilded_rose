1fe1d1bc2038172cc9687ddba3b0ac06b01af870
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-02 11:40:15 +0100

    refactor: Quitar init, cambiar import

--- app/sulfuras.py
+++ app/sulfuras.py
@@ -1,11 +1,7 @@
-from gilded_rose import Normal_item
+from normal_item import Normal_item
 
 
 class Sulfuras(Normal_item):
-    def __init__(self, sell_in, quality):
-        self.sell_in = sell_in
-        self.quality = quality
-
     def set_sell_in(self):
         assert self.sell_in == 0
         return self.sell_in
