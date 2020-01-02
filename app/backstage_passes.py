23403324ad32e1f4fca0be259c4ad339ef1e0df7
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-02 11:29:27 +0100

    refactor: Cambiar import, eliminar init

--- app/backstage_passes.py
+++ app/backstage_passes.py
@@ -1,11 +1,7 @@
-from gilded_rose import Normal_item
+from normal_item import Normal_item
 
 
 class Backstage_passes(Normal_item):
-    def __init__(self, sell_in, quality):
-        self.sell_in = sell_in
-        self.quality = quality
-
     def update_quality(self):
         if self.sell_in < 10 and self.sell_in > 5:
             self.quality + 2
