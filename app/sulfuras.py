dc69b18a390928b1f9c77580fa5f90e75b2a8e78
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-04 11:25:16 +0100

    fix: Añadir init

--- app/sulfuras.py
+++ app/sulfuras.py
@@ -2,6 +2,11 @@ from normal_item import Normal_item
 
 
 class Sulfuras(Normal_item):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
     def set_sell_in(self):
         assert self.sell_in == 0
         return self.sell_in
