7baa94ebb0eabc021e893b7ad041651ef0644326
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-04 11:24:58 +0100

    fix: Añadir init

--- app/normal_item.py
+++ app/normal_item.py
@@ -2,6 +2,11 @@ from item import Item
 
 
 class Normal_item(Item):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
     def set_sell_in(self):
         self.sell_in -= 1
 
