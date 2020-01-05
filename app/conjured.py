40413442bb3bc26d5e5c65e865ac328ab2405e38
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-05 12:26:55 +0100

    fix: Añadir init

--- app/conjured.py
+++ app/conjured.py
@@ -2,6 +2,11 @@ from normal_item import Normal_item
 
 
 class Conjured(Normal_item):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
     def update_quality(self):
         if self.sell_in > 0:
             self.quality - 2
