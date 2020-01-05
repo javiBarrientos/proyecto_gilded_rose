fe6a915b7be7a0e78125fe20a4d37b625ea77150
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-05 13:12:04 +0100

    refactor: Cambiar logica

--- app/aged_brie.py
+++ app/aged_brie.py
@@ -10,7 +10,5 @@ class Aged_brie(Normal_item):
     def update_quality(self):
         if self.sell_in > 0:
             self.quality + 1
-            return self.quality
         else:
             self.quality + 2
-            return self.quality
