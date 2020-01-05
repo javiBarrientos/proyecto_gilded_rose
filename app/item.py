f53e46af81aba87e0c3b381613da8209c3bb417e
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-05 13:00:48 +0100

    refactor: Eliminar funciones extrañas

--- app/item.py
+++ app/item.py
@@ -4,14 +4,5 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
-    def get_name(self):
-        return self.name
-
-    def get_sell_in(self):
-        return self.sell_in
-
-    def get_quality(self):
-        return self.quality
-
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
