08b9f7014b4e08f511de39c3520d4b03bc28311c
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 11:29:31 +0100

    test: Añadir test 10 dias

--- app/test_aged_brie.py
+++ app/test_aged_brie.py
@@ -4,4 +4,11 @@ from aged_brie import Aged_brie
 def test_primer_caso():
     pato = Aged_brie("pato", 10, 0)
     pato.update_item()
-    assert pato.updated_item() == ("pato", 9, 0)
+    assert pato.updated_item() == ("pato", 9, 1)
+
+
+def test_segundo_caso():
+    pato = Aged_brie("pato", 10, 0)
+    for x in range(1, 11):
+        pato.update_item()
+    assert pato.updated_item() == ("pato", 0, 10)
