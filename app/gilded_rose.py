591ddacb1661c84a127f3b29f60285270988e605
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 16:16:04 +0100

    feat: Añadir impots

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -1,3 +1,11 @@
+from item import Item
+from normal_item import Normal_item
+from aged_brie import Aged_brie
+from backstage_passes import Backstage_passes
+from sulfuras import Sulfuras
+from conjured import Conjured
+
+
 class Gilded_rose(object):
 
     def __init__(self, items):
