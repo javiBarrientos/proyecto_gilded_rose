05c967851775a34b892a8e7c27e6a65d9d279f1d
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 12:15:26 +0100

    refactor: Añadir iguales

--- app/conjured.py
+++ app/conjured.py
@@ -9,8 +9,8 @@ class Conjured(Normal_item):
 
     def update_quality(self):
         if self.sell_in > 0:
-            self.quality - 2
+            self.quality -= 2
             return self.quality
         else:
-            self.quality - 2
+            self.quality -= 2
             return self.quality
