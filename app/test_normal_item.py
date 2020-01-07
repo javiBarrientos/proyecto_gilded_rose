cd397eb7efda5d46d5576a3fc4169e872926122d
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 12:31:51 +0100

    test: Añadir test

--- app/test_normal_item.py
+++ app/test_normal_item.py
@@ -3,5 +3,5 @@ from normal_item import Normal_item
 
 def test_primer_caso():
     maleta = Normal_item("maleta", 10, 0)
-    maleta.update_quality()
-    maleta.set_quality(10)
+    maleta.update_item()
+    assert maleta.updated_item() == ("maleta", 9, 0)
