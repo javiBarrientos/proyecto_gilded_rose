9c337144855ec910fa3b6b1f7ec715d70820d0d7
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 12:29:31 +0100

    test: Añadir test

--- app/test_sulfuras.py
+++ app/test_sulfuras.py
@@ -5,3 +5,10 @@ def test_primer_caso():
     azufre = Sulfuras("azufre", 0, 80)
     azufre.update_item()
     assert azufre.updated_item() == ("azufre", 0, 80)
+
+
+def test_segundo_caso():
+    azufre = Sulfuras("azufre", 0, 80)
+    for x in range(1, 40):
+        azufre.update_item()
+    assert azufre.updated_item() == ("azufre", 0, 80)
