1101133fe900acbb0499d0eb896dfc784e39f4ec
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 12:20:05 +0100

    test: Añadir test

--- app/test_conjured.py
+++ app/test_conjured.py
@@ -2,6 +2,13 @@ from conjured import Conjured
 
 
 def test_primer_caso():
-    fetiche_sonrriente = Conjured("fetiche_sonrriente", 10, 0)
-    fetiche_sonrriente.update_quality()
-    fetiche_sonrriente.set_quality(9)
+    varita = Conjured("varita", 10, 10)
+    varita.update_item()
+    assert varita.updated_item() == ("varita", 9, 8)
+
+
+def test_segundo_caso():
+    varita = Conjured("varita", 10, 10)
+    for x in range(1, 11):
+        varita.update_item()
+    assert varita.updated_item() == ("varita", 0, 0)
