a25b4b825608dd6e7eb2502709c0a84cbd2b09b5
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 11:02:01 +0100

    feat: Refactor + new feat
    Añadir return de funciones, añadir funcion de update_item

--- app/normal_item.py
+++ app/normal_item.py
@@ -9,15 +9,22 @@ class Normal_item(Item):
 
     def update_sell_in(self):
         self.sell_in -= 1
+        return self.sell_in
 
     def update_quality(self):
         if self.sell_in > 0:
             self.quality -= 1
         else:
             self.quality -= 2
+        return self.quality
 
     def check_quality(self):
         if self.quality < 0:
             self.quality = 0
         if self.quality > 50:
             self.quality = 50
+
+    def update_item(self):
+        Normal_item.update_sell_in(self)
+        Normal_item.update_quality(self)
+        Normal_item.check_quality(self)
