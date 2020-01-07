e2bfe4e8d41d1e3c08dbf2b78fcf0512fd8b04bd
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 12:12:36 +0100

    test: Añadir test

--- app/test_backstage_passes.py
+++ app/test_backstage_passes.py
@@ -2,6 +2,20 @@ from backstage_passes import Backstage_passes
 
 
 def test_primer_caso():
-    architects = Backstage_passes("architects", 10, 0)
-    architects.update_quality()
-    architects.set_quality(20)
+    concierto = Backstage_passes("concierto", 10, 0)
+    concierto.update_item()
+    assert concierto.updated_item() == ("concierto", 9, 2)
+
+
+def test_segundo_caso():
+    concierto = Backstage_passes("concierto", 10, 0)
+    for x in range(1, 11):
+        concierto.update_item()
+    assert concierto.updated_item() == ("concierto", 0, 12)
+
+
+def test_tercer_caso():
+    concierto = Backstage_passes("concierto", 10, 0)
+    for x in range(1, 16):
+        concierto.update_item()
+    assert concierto.updated_item() == ("concierto", -5, 0)
