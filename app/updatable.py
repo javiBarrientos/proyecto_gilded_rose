512da0875fd62cbc91fba3abaf6b374d3c045f2e
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-07 15:16:33 +0100

    feat: Añadir modulo updatable

---
+++ app/updatable.py
@@ -0,0 +1,8 @@
+class Updatable():
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def update_quality(self):
+        pass
