af4a1b748112136be614b24f0033ab228437abe8
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-09 00:25:09 +0100

    feat: Añadir ruta inventario

--- app/app.py
+++ app/app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/inventario')
+def inventario():
+    return 'eis'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
