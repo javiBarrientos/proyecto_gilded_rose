60ef1f7ea36dc0434fafc241c233ef46ef8d6438
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-08 12:31:49 +0100

    feat: Añadir items
    Añadir funcion items

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -6,7 +6,7 @@ from sulfuras import Sulfuras
 from conjured import Conjured
 
 
-class Gilded_rose(object):
+class Gilded_rose(object, Normal_item):
 
     def __init__(self, items):
         self.items = items
@@ -14,6 +14,27 @@ class Gilded_rose(object):
     def update_quality(self):
         for item in self.inventario:
             self.update_quality()
+
+    def items(self):
+        lista_items = Gilded_rose([
+            Normal_item("+ 5 Dexterity Vest", 10, 20),
+            Aged_brie("Aged Brie", 2, 0),
+            Normal_item("Elixir of the Mongoose", 5, 7),
+            Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+            Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+            Backstage_passes(
+                "Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Backstage_passes(
+                "Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            Backstage_passes(
+                "Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            Conjured("Conjured Mana Cake", 3, 6)])
+        for day in range(1, 31):
+            lista_items.update_quality()
+        return lista_items.updated_item()
+
+    def get_item(self):
+        return self.items
 
 
 if __name__ == "__main__":
