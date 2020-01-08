14ee84c105711d7223ac04ce40b94c6323335d05
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-08 15:40:26 +0100

    test: Añadir test

--- app/test_gilded_rose.py
+++ app/test_gilded_rose.py
@@ -1,18 +1,15 @@
-'''
-# -*- coding: utf-8 -*-
-import unittest
-
-from gilded_rose import Item, GildedRose
+from gilded_rose import Gilded_rose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-
-
-if __name__ == '__main__':
-    unittest.main()
-'''
+def test_primer_caso():
+    lista_items = Gilded_rose([
+        Normal_item("+ 5 Dexterity Vest", 10, 20),
+        Aged_brie("Aged Brie", 2, 0),
+        Normal_item("Elixir of the Mongoose", 5, 7),
+        Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+        Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+        Backstage_passes("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        Backstage_passes("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        Backstage_passes("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        Conjured("Conjured Mana Cake", 3, 6)])
+    for day in range(1, 31):
