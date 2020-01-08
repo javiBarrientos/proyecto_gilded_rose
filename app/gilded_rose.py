629615d7c4aec609563bbd652e6a86471a10797c
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-08 20:25:39 +0100

    feat: Añadir update_quality, print, updated_items
    He añadido varias features

--- app/gilded_rose.py
+++ app/gilded_rose.py
@@ -1,3 +1,5 @@
+from item import Item
+from normal_item import Normal_item
 from aged_brie import Aged_brie
 from backstage_passes import Backstage_passes
 from sulfuras import Sulfuras
@@ -10,29 +12,11 @@ class Gilded_rose(object):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
-            item.update_quality()
-            self.update_quality()
-
-    def items(self):
-        lista_items = Gilded_rose([
-            Normal_item("+ 5 Dexterity Vest", 10, 20),
-            Aged_brie("Aged Brie", 2, 0),
-            Normal_item("Elixir of the Mongoose", 5, 7),
-            Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
-            Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
-            Backstage_passes(
-                "Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            Backstage_passes(
-                "Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            Backstage_passes(
-                "Backstage passes to a TAFKAL80ETC concert", 5, 49),
-            Conjured("Conjured Mana Cake", 3, 6)])
         for day in range(1, 31):
-            lista_items.update_quality()
-        return lista_items.updated_item()
+            for item in self.items:
+                item.update_item()
 
-    def get_item(self):
+    def updated_items(self):
         return self.items
 
 
@@ -41,9 +25,11 @@ if __name__ == "__main__":
         Normal_item("+ 5 Dexterity Vest", 10, 20),
         Aged_brie("Aged Brie", 2, 0),
         Normal_item("Elixir of the Mongoose", 5, 7),
-        Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+        Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
         Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
         Backstage_passes("Backstage passes to a TAFKAL80ETC concert", 15, 20),
         Backstage_passes("Backstage passes to a TAFKAL80ETC concert", 10, 49),
         Backstage_passes("Backstage passes to a TAFKAL80ETC concert", 5, 49),
         Conjured("Conjured Mana Cake", 3, 6)])
+    lista_items.update_quality()
+    print(lista_items.updated_items())
