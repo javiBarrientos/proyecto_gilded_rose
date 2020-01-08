671f95a411750eef7ec1c324abe19c261662da41
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-08 09:17:51 +0100

    test: Añadir test

--- app/test_conjured.py
+++ app/test_conjured.py
@@ -12,3 +12,10 @@ def test_segundo_caso():
     for x in range(1, 11):
         varita.update_item()
     assert varita.updated_item() == ("varita", 0, 0)
+
+
+def test_tercer_caso():
+    sombrero = Conjured("sombrero", 25, 30)
+    for x in range(1, 31):
+        sombrero.update_item()
+    assert sombrero.updated_item() == ("sombrero", -5, 0)
