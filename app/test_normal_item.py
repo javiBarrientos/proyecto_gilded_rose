50237d2f331a5a860d53afbfa105fedd031d8811
Javi <jbarrientos@cifpfbmoll.eu>
2020-01-08 09:12:30 +0100

    test: Añadir test

--- app/test_normal_item.py
+++ app/test_normal_item.py
@@ -5,3 +5,10 @@ def test_primer_caso():
     maleta = Normal_item("maleta", 10, 0)
     maleta.update_item()
     assert maleta.updated_item() == ("maleta", 9, 0)
+
+
+def test_segundo_caso():
+    taza = Normal_item("taza", 10, 10)
+    for x in range(1, 21):
+        taza.update_item()
+    assert taza.updated_item() == ("taza", -10, 0)
