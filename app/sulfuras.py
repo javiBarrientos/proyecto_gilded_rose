b28d955ad3503494836114cdca900da02bb7acc1
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-08 22:31:28 +0100

    refact:Añadir asserts

--- app/sulfuras.py
+++ app/sulfuras.py
@@ -4,8 +4,11 @@ from normal_item import Normal_item
 class Sulfuras(Normal_item):
     def __init__(self, name, sell_in, quality):
         self.name = name
+        assert isinstance(name, str)
         self.sell_in = sell_in
+        assert isinstance(sell_in, int)
         self.quality = quality
+        assert isinstance(quality, int)
 
     def update_sell_in(self):
         pass
--- app/updatable.py
+++ app/updatable.py
@@ -1,8 +1,11 @@
 class Updatable():
     def __init__(self, name, sell_in, quality):
         self.name = name
+        assert isinstance(name, str)
         self.sell_in = sell_in
+        assert isinstance(sell_in, int)
         self.quality = quality
+        assert isinstance(quality, int)
 
     def update_quality(self):
         pass
