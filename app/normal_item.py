cfa66acc11cec12cc67e2c96f5940202530b4f87
samuFerragut <sferragut@cifpfbmoll.eu>
2020-01-08 17:53:00 +0100

    fix: Crear precondiciones de items pasados

--- app/normal_item.py
+++ app/normal_item.py
@@ -5,8 +5,11 @@ from updatable import Updatable
 class Normal_item(Item, Updatable):
     def __init__(self, name, sell_in, quality):
         self.name = name
+        assert isinstance(name, str)
         self.sell_in = sell_in
+        assert isinstance(sell_in, int)
         self.quality = quality
+        assert isinstance(quality, int)
 
     def update_sell_in(self):
         self.sell_in -= 1
