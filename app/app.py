178e0e4b0c58d1b02e8deafb21b81dc4fe37f904
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-02-12 19:35:30 +0100

    Cambiar espaciado

--- app/app.py
+++ app/app.py
@@ -8,22 +8,17 @@ from conjured import Conjured
 from flask import render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/inventario/')
 def inventario():
     return render_template('inventario.html')
 
-
 @app.route('/updateInventario/')
 def updateInventario():
     return render_template('updateInventario.html')
-    return 'Lista de objetos en el inventario'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
